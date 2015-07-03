62cae1c6ec5c1494abfe1d5e03f85ab7f865bf03
Arvind Kumar <arvind.kumar@ArvMac.local>
2015-07-03 16:34:01 +0530

    attachment

--- index.js
+++ index.js
@@ -26,13 +26,15 @@ io.on('connection', function(client){
         io.sockets.emit("chat", people[client.id], msg);
     });
 
-
+    client.on('user image', function (msg) {
+      client.broadcast.emit('user image', people[client.id], msg);
+    });
 	client.on("disconnect", function(){
         io.sockets.emit("update", people[client.id] + " has left the server.");
         delete people[client.id];
         io.sockets.emit("update-people", people);
     });
-  
+
 });
 
 io.on('disconnect', function(){
